3db846a4cf25b9b80842a3d15bf49152fc9e5f97
Ignacio Carrasco <igna.carrasco@duocuc.cl>
2023-07-04 21:32:54 -0400

    Cambios index

--- web_CleanServe/Controllers/HomeController.cs
+++ web_CleanServe/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace web_CleanServe.Controllers
             using (var dbContext = new SERVICIO_LIMPIEZAEntities())
             {
                 ViewBag.Productos = dbContext.Producto.Take(20).ToList(); //solo los 20 primeros
+                ViewBag.Proveedor = dbContext.Proveedor.Take(3).ToList(); //solo los 3 primeros
                 return View();
             }
         }

215c53d1239e637bb3d64ab9eb66996c8969a996
Ignacio Carrasco <igna.carrasco@duocuc.cl>
2023-07-13 22:29:36 -0400

    CRUD Clientes

--- app_CleanServ/View/ClientesForms.cs
+++ app_CleanServ/View/ClientesForms.cs
@@ -1,4 +1,6 @@
 ﻿using app_CleanServ.Controllers;
+using DataAccess.Models;
+using MetroFramework;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,8 +26,15 @@ namespace app_CleanServ.View
 
         private void ClientesForms_Load(object sender, EventArgs e)
         {
+            dataGridView1.Visible = false;
+            CargarClientes();
+        }
+
+        private void CargarClientes()
+        {
             try
             {
+                dataGridView1.Columns.Clear(); // Limpiar las columnas existentes
                 dataGridView1.DataSource = clientesController.getClientes();
                 dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
                 dataGridView1.ReadOnly = true;
@@ -42,11 +51,71 @@ namespace app_CleanServ.View
                 dataGridView1.Columns["direccion"].Visible = false;
                 dataGridView1.Columns["solicitud"].Visible = false;
 
+                // Agregar columna de botón para eliminar
+                DataGridViewButtonColumn eliminarButtonColumn = new DataGridViewButtonColumn();
+                eliminarButtonColumn.Name = "Eliminar";
+                eliminarButtonColumn.Text = "Eliminar";
+                eliminarButtonColumn.UseColumnTextForButtonValue = true;
+                dataGridView1.Columns.Add(eliminarButtonColumn);
+
+                // Agregar columna de botón para modificar
+                DataGridViewButtonColumn modificarButtonColumn = new DataGridViewButtonColumn();
+                modificarButtonColumn.Name = "Modificar";
+                modificarButtonColumn.Text = "Modificar";
+                modificarButtonColumn.UseColumnTextForButtonValue = true;
+                dataGridView1.Columns.Add(modificarButtonColumn);
+                dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+                dataGridView1.Visible = true;
+
             }
             catch (Exception ex)
             {
                 // Mostrar un mensaje de error al usuario
                 MetroFramework.MetroMessageBox.Show(this, "Error al cargar los clientes: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Verificar si se hizo clic en el botón de eliminar
+            if (e.ColumnIndex == dataGridView1.Columns["Eliminar"].Index && e.RowIndex >= 0)
+            {
+                // Mostrar mensaje de confirmación
+                DialogResult result = MetroMessageBox.Show(this, "¿Estás seguro de eliminar este cliente?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Obtener el ID del cliente de la fila seleccionada
+                    int ID_cliente = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["ID_cliente"].Value);
+
+                    // Llamar al método removeCliente del controlador para eliminar el cliente
+                    clientesController.removeCliente(ID_cliente);
+
+                    // Volver a cargar los cliente en el DataGridView
+                    CargarClientes();
+                }
+            }
+            // Verificar si se hizo clic en el botón de modificar
+            else if (e.ColumnIndex == dataGridView1.Columns["Modificar"].Index && e.RowIndex >= 0)
+            {
+                // Obtener el ID del cliente de la fila seleccionada
+                int ID_cliente = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["ID_cliente"].Value);
+
+                // Obtener el cliente correspondiente al ID_cliente
+                Cliente cliente = clientesController.getCliente(ID_cliente);
+
+                // Verificar si el cliente existe
+                if (cliente != null)
+                {
+                    // Crear un formulario de modificación
+                    ModificarClienteForm modificarForm = new ModificarClienteForm(cliente);
+
+                    // Mostrar el formulario de modificación
+                    modificarForm.ShowDialog();
+
+                    // Volver a cargar los cliente en el DataGridView
+                    CargarClientes();
+                }
             }
         }
     }
---
+++ app_CleanServ/View/ModificarClienteForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace app_CleanServ.View
+{
+    partial class ModificarClienteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnGuardar = new MetroFramework.Controls.MetroButton();
+            this.lblIdCliente = new MetroFramework.Controls.MetroLabel();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 102F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.btnGuardar, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.lblIdCliente, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtID, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtDNI, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtNombre, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtApellido, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel4, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtEmail, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.metroLabel5, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.txtCelular, 1, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 106);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.44526F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.55474F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 74F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 92F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(639, 424);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(69)))), ((int)(((byte)(113)))));
+            this.btnGuardar.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnGuardar.Location = new System.Drawing.Point(540, 390);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(94, 28);
+            this.btnGuardar.Style = MetroFramework.MetroColorStyle.White;
+            this.btnGuardar.TabIndex = 20;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.btnGuardar.UseCustomBackColor = true;
+            this.btnGuardar.UseSelectable = true;
+            this.btnGuardar.UseStyleColors = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblIdCliente
+            // 
+            this.lblIdCliente.AutoSize = true;
+            this.lblIdCliente.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblIdCliente.Location = new System.Drawing.Point(3, 0);
+            this.lblIdCliente.Name = "lblIdCliente";
+            this.lblIdCliente.Size = new System.Drawing.Size(73, 19);
+            this.lblIdCliente.TabIndex = 0;
+            this.lblIdCliente.Text = "ID Cliente";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(182, 3);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 68);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(34, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "DNI";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(182, 71);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(100, 20);
+            this.txtDNI.TabIndex = 3;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel2.Location = new System.Drawing.Point(3, 144);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(65, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(182, 147);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel3.Location = new System.Drawing.Point(3, 218);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(66, 19);
+            this.metroLabel3.TabIndex = 6;
+            this.metroLabel3.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(182, 221);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel4.Location = new System.Drawing.Point(3, 295);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(45, 19);
+            this.metroLabel4.TabIndex = 8;
+            this.metroLabel4.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(182, 298);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel5.Location = new System.Drawing.Point(3, 387);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(56, 19);
+            this.metroLabel5.TabIndex = 10;
+            this.metroLabel5.Text = "Celular";
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(182, 390);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(100, 20);
+            this.txtCelular.TabIndex = 11;
+            // 
+            // ModificarClienteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 563);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ModificarClienteForm";
+            this.Text = "Modificar Cliente";
+            this.Load += new System.EventHandler(this.ModificarClienteForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MetroFramework.Controls.MetroLabel lblIdCliente;
+        private System.Windows.Forms.TextBox txtID;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.TextBox txtDNI;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private System.Windows.Forms.TextBox txtApellido;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.TextBox txtCelular;
+        private MetroFramework.Controls.MetroButton btnGuardar;
+    }
+}
